243a67e6460210b84598e92e8048e076edb92fef
lhqezio <lhqezio1010@gmail.com>
2022-09-21 10:50:17 -0400

    Comments update

--- js/table_generator.js
+++ js/table_generator.js
@@ -8,6 +8,7 @@ function generateTable(){
     let row_num = Number(document.getElementById('rcount').value);
     let table_html = document.createElement('textarea');
     table_html.innerHTML = "<table>"
+    //Go through the number of row and col to create the quantity of td in tr desired while making a string for the textare field
     for (let i = 0;i<col_num;i++){
         let  tr_node = document.createElement('tr');
         table_html.innerHTML += "\n\t<tr>";
@@ -21,9 +22,11 @@ function generateTable(){
         table_node.appendChild(tr_node);
     }
     table_html.innerHTML += "\n</table>"
+    //Make the textarea read only in order to prevent input eventListener from being executed from changing it
     table_html.setAttribute('readonly', '');
     table_render.replaceChildren(table_node);
     table_html_render.replaceChildren(table_html);
+    //Run input changes in order to change css properties
     inputChanges();
 }
 function inputChanges(){
@@ -32,8 +35,10 @@ function inputChanges(){
     let bcolor = document.getElementById("bcolor").value;
     let twidth = document.getElementById("twidth").value;
     let bwidth = document.getElementById("bwidth").value;
+    //Change CSS props for table and all its children
     let elem = document.querySelectorAll("table,td,tr");
     let table = document.querySelector("table");
+    //Tag width is only for table
     utilities.changeTagWidth(table,twidth);
     for (let i = 0; i < elem.length; i++) {
         utilities.changeBackground(elem[i],bg_color);
